7e76e93b29110355c082515dcfc4f8dd9379e975
Nakru <nakru03@gmail.com>
2022-03-06 20:52:21 +0900

    Add UnitTest Demo code.

---
+++ src/main/java/com/nakru03/spring/springboot/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.nakru03.spring.springboot.web;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
